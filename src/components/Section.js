96bc342d5a6a2362070888c15e6545b9bafb487f
Amit Pandey <87856042+Amit89480@users.noreply.github.com>
2022-10-22 23:54:28 +0530

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,73 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section(props) {
+function Header() {
   return (
-    <Wrap bgImage={props.backgroundImg}>
-      <ItemText>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-      </ItemText>
-      <Buttons>
-      <ButtonGroup>
-        <LeftButton>{props.leftBtnText}</LeftButton>
-        {props.rightBtnText && <RightButton>{props.rightBtnText}</RightButton>}
-        
-      </ButtonGroup>
-      <DownArrow src="/images/down-arrow.svg" />
-      </Buttons>
-    </Wrap>
+    <Container>
+    
+        <img src="images/logo.svg" alt="" />
+   
+      <Menu>
+        <a href="/">Model S</a>
+        <a href="/">Model 3</a>
+        <a href="/">Model X</a>
+        <a href="/">Model Y</a>
+        <a href="/">Solar Roof</a>
+        <a href="/">Solar Panels</a>
+      </Menu>
+      <RightMenu>
+        <a href="/"> Shop</a>
+        <a href="/"> Account</a>
+        <a href="/"> Menu</a>
+      </RightMenu>
+    </Container>
   );
 }
 
-export default Section;
+export default Header;
 
-const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  display:flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background-image: ${props => `url("/images/${props.bgImage}")`}
-  `;
-const ItemText = styled.div`
-  padding-top: 15vh;
-  text-align: center;
-`;
-const ButtonGroup = styled.div`
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
   display: flex;
-  margin-bottom: 30px;
-  @media (max-width:768px){
-    flex-direction: column;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
-const LeftButton = styled.div`
-  background-color: rgba(23, 26, 32, 0.8);
-  height: 40px;
-  width: 256px;
-  color: white;
+const RightMenu = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  border-radius: 100px;
-  opacity: 0.85;
-  cursor: pointer;
-  text-transform: uppercase;
-  font-size: 12px;
-  margin: 8px;
-`;
-const RightButton = styled(LeftButton)``;
-
-const DownArrow = styled.img`
-    height: 40px;
-    overflow-x: hidden;
-    animation: animateDown infinite 1.5s;
-
-    `;
-const Buttons = styled.div`
-
+  margin-right: 10px;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
 `;
